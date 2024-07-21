6ba854891e9ee5822a3a09ffded8c93854f6e809
Marco Rizk <marco.samir@titrias.com>
2024-07-21 20:12:10 +0300

    spike

---
+++ src/app/single-employee/single-employee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-employee',
+  templateUrl: './single-employee.component.html',
+  styleUrls: ['./single-employee.component.scss']
+})
+export class SingleEmployeeComponent {
+
+}
