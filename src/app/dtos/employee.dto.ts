75dccb3480a7c43a0d2e7dcd941cdf5f84553be6
Marco Rizk <marco.samir@titrias.com>
2024-07-22 19:30:06 +0300

    adding dto

---
+++ src/app/dtos/employee.dto.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+    serial: number;
+    status: 'active' | 'inactive';
+    name: string;
+    userSerial: number;
+    userAuthority: string;
+    function: string;
+    mobile: string;
+  }
