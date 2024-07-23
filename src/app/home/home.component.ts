9b3e14bebf4671aa98682132ef709c76e990dd3f
Marco Rizk <marco.samir@titrias.com>
2024-07-23 15:53:36 +0300

    adding sorting

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { Employee } from '../dtos/employee.dto';
 export class HomeComponent implements OnInit {
   allEmployees: Employee[] = [];
   selectedEmployees: Employee[] = [];
+  sorting=''
   constructor(private employeesService: EmployeesService) {}
   ngOnInit(): void {
     this.employeesService.getEmployees().subscribe((result) => {
@@ -29,5 +30,52 @@ export class HomeComponent implements OnInit {
         (selected) => selected.serial !== employee.serial
       );
     }
+  }
+
+  sortBy(type: string) {
+    if(type === this.sorting){
+      this.allEmployees.reverse();
+    } 
+    else if (type === 'name') {
+      this.allEmployees.sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      });
+    } else if (type === 'serial') {
+      this.allEmployees.sort((a, b) => a.serial - b.serial);
+    } else if (type === 'status') {
+      this.allEmployees.sort((a, b) => {
+        if (a.status < b.status) return -1;
+        if (a.status > b.status) return 1;
+        return 0;
+      });
+    } else if (type === 'userSerial') {
+      this.allEmployees.sort((a, b) => a.userSerial - b.userSerial);
+    } else if (type === 'userAuthority') {
+      this.allEmployees.sort((a, b) => {
+        if (a.userAuthority < b.userAuthority) return -1;
+        if (a.userAuthority > b.userAuthority) return 1;
+        return 0;
+      });
+    } else if (type === 'function') {
+      this.allEmployees.sort((a, b) => {
+        if (a.function < b.function) return -1;
+        if (a.function > b.function) return 1;
+        return 0;
+      });
+    } else if (type === 'vip') {
+      this.allEmployees.sort((a, b) => {
+        if (a.vip === b.vip) return 0;
+        return a.vip ? -1 : 1;
+      });
+    } else if (type === 'mobile') {
+      this.allEmployees.sort((a, b) => {
+        if (a.mobile < b.mobile) return -1;
+        if (a.mobile > b.mobile) return 1;
+        return 0;
+      });
+    }
+    this.sorting = type
   }
 }
