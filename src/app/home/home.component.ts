91cd5d592de5afb8266c01ec000c64d935bd6fd5
Marco Rizk <marco.samir@titrias.com>
2024-07-25 02:38:55 +0300

    sorting resolving

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -115,42 +115,42 @@ export class HomeComponent implements OnInit {
 
   sortBy(type: string) {
     if (type === this.sorting) {
-      this.allEmployees.reverse();
+      this.shownEmployees.reverse();
     } else if (type === 'name') {
-      this.allEmployees.sort((a, b) => {
+      this.shownEmployees.sort((a, b) => {
         if (a.name < b.name) return -1;
         if (a.name > b.name) return 1;
         return 0;
       });
     } else if (type === 'serial') {
-      this.allEmployees.sort((a, b) => a.serial - b.serial);
+      this.shownEmployees.sort((a, b) => a.serial - b.serial);
     } else if (type === 'status') {
-      this.allEmployees.sort((a, b) => {
+      this.shownEmployees.sort((a, b) => {
         if (a.status < b.status) return -1;
         if (a.status > b.status) return 1;
         return 0;
       });
     } else if (type === 'userSerial') {
-      this.allEmployees.sort((a, b) => a.userSerial - b.userSerial);
+      this.shownEmployees.sort((a, b) => a.userSerial - b.userSerial);
     } else if (type === 'userAuthority') {
-      this.allEmployees.sort((a, b) => {
+      this.shownEmployees.sort((a, b) => {
         if (a.userAuthority < b.userAuthority) return -1;
         if (a.userAuthority > b.userAuthority) return 1;
         return 0;
       });
     } else if (type === 'function') {
-      this.allEmployees.sort((a, b) => {
+      this.shownEmployees.sort((a, b) => {
         if (a.function < b.function) return -1;
         if (a.function > b.function) return 1;
         return 0;
       });
     } else if (type === 'vip') {
-      this.allEmployees.sort((a, b) => {
+      this.shownEmployees.sort((a, b) => {
         if (a.vip === b.vip) return 0;
         return a.vip ? -1 : 1;
       });
     } else if (type === 'mobile') {
-      this.allEmployees.sort((a, b) => {
+      this.shownEmployees.sort((a, b) => {
         if (a.mobile < b.mobile) return -1;
         if (a.mobile > b.mobile) return 1;
         return 0;
